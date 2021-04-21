a74da395c49f6397b9e88e44c05a9a27321fb3bb
jaeyoonlee2 <>
2021-04-21 03:36:28 +0000

    initial commit

---
+++ package-info.java
@@ -0,0 +1,9 @@
+/**
+* The Hello World! program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* @author  Jay Lee
+* @version 1.0
+* @since   2020-04-20
+*/
+package ca.mths.intro.intro_01.java.hello_world;
